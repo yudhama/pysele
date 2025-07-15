5da0f0fad9c397586f03b890106d2116533c95a6
yudhama <yudha.m.a@gmail.com>
2025-07-15 16:43:54 +0700

    add new sample test using csv

---
+++ cobaTest/tests/sample_csv.py
@@ -0,0 +1,37 @@
+import csv
+from cobaTest.pages.login_page import LoginPage
+from cobaTest.pages.appointment_page import AppointmentPage
+from cobaTest.pages.confirmation_page import ConfirmationPage
+from cobaTest.pages.menu_page import MenuPage
+from cobaTest.utils.driver_factory import get_driver
+
+def test_make_appointment_from_csv():
+    with open('cobaTest/files/credentials.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            driver = get_driver(headless=False)
+            driver.get("https://katalon-demo-cura.herokuapp.com/")
+
+            login_page = LoginPage(driver)
+            appointment_page = AppointmentPage(driver)
+            confirmation_page = ConfirmationPage(driver)
+            menu_page = MenuPage(driver)
+
+            login_page.go_to_login()
+            login_page.login(row['username'], row['password'])
+
+            config = {
+                "facility": "Seoul CURA Healthcare Center",
+                "readmission": True,
+                "healthcare_program": "Medicaid",
+                "visit_date": "25/07/2025",
+                "comment": "Follow-up appointment for check-up"
+            }
+            appointment_page.book_appointment(config)
+            details = confirmation_page.get_details()
+            print(f"User: {row['username']} - Details: {details}")
+            menu_page.logout()
+            driver.quit()
+
+if __name__ == "__main__":
+    test_make_appointment_from_csv()
